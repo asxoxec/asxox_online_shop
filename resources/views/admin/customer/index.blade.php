31923328fcfa27fc6b3d214503aaef57c91bc138
asxox_onlineshop <asxoxec@gmail.com>
2021-01-25 20:57:19 -0800

    customer

---
+++ resources/views/admin/customer/index.blade.php
@@ -0,0 +1,77 @@
+@extends('admin.site.master')
+
+@section('content')
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-4">
+                    <h1 class="m-0 text-dark">Customer</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-4">
+                    @include('admin.error')
+                </div><!-- /.col -->
+                <div class="col-sm-4">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Customer / Home</a></li>
+                        <!-- <li class="breadcrumb-item active">Dashboard v1</li> -->
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+
+                <div class="col-lg-12 col-md-12 col-12 bg-white">
+
+                    <div class="card-body">
+                        
+
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Name</th>
+                                    <th>Phone</th>
+                                    <th>Address</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php $i=1; @endphp
+                                @foreach($customers as $customer)
+                                <tr>
+                                    <td>@php echo $i;$i++; @endphp</td>
+                                    <td>{{$customer->name}}</td>
+                                    <td>{{$customer->phone}}</td>
+                                    <td>{{$customer->address}}</td>
+                                    
+                                </tr>
+
+                                @endforeach
+
+                            </tbody>
+
+                        </table>
+
+                    </div>
+                </div>
+
+            </div>
+            <!-- /.row -->
+
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+
+
+
+@endsection
